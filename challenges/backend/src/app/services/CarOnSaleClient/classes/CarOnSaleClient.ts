42d4bf0ab1d1744ea9c15635f3d84dc078756044
Hadrian Węgrzynowski <hadrian@hawski.com>
2019-08-01 22:45:14 +0200

    Add preliminary code for getRunningAuctions implementation

--- challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.ts
+++ challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.ts
@@ -4,6 +4,7 @@ import {ICarOnSaleClient} from "../interface/ICarOnSaleClient";
 import {DependencyIdentifier} from "../../../DependencyIdentifiers";
 import "reflect-metadata";
 import {createHash} from "crypto";
+import fetch from "node-fetch";
 
 @injectable()
 export class CarOnSaleClient implements ICarOnSaleClient {
@@ -24,7 +25,45 @@ export class CarOnSaleClient implements ICarOnSaleClient {
     }
 
     public async getRunningAuctions(): Promise<void /* TODO: Introduce a type */> {
+        const hashpass = this.hashPasswordWithCycles("123test", 5);
+        const user = encodeURIComponent("salesman@random.com");
+        const headers = {
+            "Accept": "application/json",
+            "User-Agent": "caronsale-coding-challenge",
+        };
 
+        const base = "https://caronsale-backend-service-dev.herokuapp.com/api/v1/";
+
+        const authResponse = await fetch(base + "authentication/" + user, {
+            method: "PUT",
+            headers: {
+                ...headers,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                password: hashpass,
+                meta: "",
+            }),
+        });
+        this.logger.log(JSON.stringify({
+            password: hashpass,
+            meta: "",
+        }));
+        this.logger.log(authResponse.url);
+
+        const auth = await authResponse.json();
+
+        this.logger.log(JSON.stringify(auth));
+        this.logger.log(Buffer.from(auth.token).toString("base64"));
+
+        const response = await fetch(base + "auction/salesman/" + user + "/_all" +
+                "?access_token=" + encodeURIComponent(Buffer.from(auth.token).toString("base64")), {
+            method: "GET",
+            headers: headers,
+        });
+        const list = await response.json();
+
+        this.logger.log(JSON.stringify(list));
     }
 
 }
