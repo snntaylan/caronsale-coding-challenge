aee8d5ac48a1fd48ad2f1554bf424312ad618211
Hadrian Węgrzynowski <hadrian@hawski.com>
2019-08-01 22:38:44 +0200

    Add password hashing function

--- challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.ts
+++ challenges/backend/src/app/services/CarOnSaleClient/classes/CarOnSaleClient.ts
@@ -3,6 +3,7 @@ import {ILogger} from "../../Logger/interface/ILogger";
 import {ICarOnSaleClient} from "../interface/ICarOnSaleClient";
 import {DependencyIdentifier} from "../../../DependencyIdentifiers";
 import "reflect-metadata";
+import {createHash} from "crypto";
 
 @injectable()
 export class CarOnSaleClient implements ICarOnSaleClient {
@@ -10,6 +11,17 @@ export class CarOnSaleClient implements ICarOnSaleClient {
     public constructor(@inject(DependencyIdentifier.LOGGER) private logger: ILogger) {
     }
 
+    private hashPasswordWithCycles(plainTextPassword: string, cycles: number): string {
+        let hash = `${plainTextPassword}`;
+
+        for(let i = 0; i < cycles; i++) {
+            const sha512 = createHash("sha512");
+            sha512.update(hash);
+            hash = sha512.digest("hex");
+        }
+
+        return hash;
+    }
 
     public async getRunningAuctions(): Promise<void /* TODO: Introduce a type */> {
 
